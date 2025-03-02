592eff2e0a7addadcf05ee691b69ccfdd33f2473
hash-bash <hash-bash@users.noreply.github.com>
2025-03-02 18:33:55 +0000

    deploy: 5d5a02b8c6530672c7b445ab1518ff2639124311

--- _nuxt/C_irzsiA.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./I2pWbZe3.js";const r=a("tools",{state:()=>({metadata:null,loading:!1,error:null}),actions:{async fetchMetadata(){this.loading=!0,this.error=null;try{this.metadata=await $fetch("/combined_metadata.json")}catch(t){this.error=t,console.error("Error fetching metadata:",t)}finally{this.loading=!1}}}});export{r as u};
